18c0e1f022f9e3dc10ca2868a97e769772b2567d
Demis Bellot <demis.bellot@gmail.com>
2017-08-08 05:44:17 -0400

    remove wrapping

--- src/TemplateWebsites/DbTemplateFilters.cs
+++ src/TemplateWebsites/DbTemplateFilters.cs
@@ -19,10 +19,10 @@ namespace TemplateWebsites
         }
 
         public object dbSelect(string sql) => exec(db => db.Select<Dictionary<string,object>>(sql));
-        public object dbSelect(string sql, Dictionary<string,object> args) => exec(db => db.Select<Dictionary<string,object>>(sql, args));
+        public object dbSelect(string sql, Dictionary<string,object> args) => exec(db => db.Select<Dictionary<string,object>>(sql,args));
         public object dbSingle(string sql) => exec(db => db.Single<Dictionary<string,object>>(sql));
-        public object dbSingle(string sql, Dictionary<string,object> args) => exec(db => db.Single<Dictionary<string,object>>(sql, args));
+        public object dbSingle(string sql, Dictionary<string,object> args) => exec(db => db.Single<Dictionary<string,object>>(sql,args));
         public object dbScalar(string sql) => exec(db => db.Scalar<object>(sql));
-        public object dbScalar(string sql, Dictionary<string,object> args) => exec(db => db.Scalar<object>(sql, args));
+        public object dbScalar(string sql, Dictionary<string,object> args) => exec(db => db.Scalar<object>(sql,args));
     }
 }
