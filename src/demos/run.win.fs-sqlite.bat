ec8e72981839f8344f16c4f24b5222fcc750d378
Demis Bellot <demis.bellot@gmail.com>
2017-08-10 13:38:53 -0400

    move demos to apps

--- src/demos/run.win.fs-sqlite.bat
+++
@@ -1,5 +0,0 @@
-COPY fs-sqlite\web.settings ..\TemplateWebsites\bin\release\netcoreapp1.1\osx.10.11-x64
-COPY fs-sqlite\web.settings ..\TemplateWebsites\bin\release\netcoreapp1.1\ubuntu.14.04-x64
-COPY fs-sqlite\web.settings ..\TemplateWebsites\bin\release\netcoreapp1.1\win10-x64
-
-..\TemplateWebsites\bin\release\netcoreapp1.1\win10-x64\web
