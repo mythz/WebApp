01844931210101df652d1fe4daa6aefd0767cfdd
Demis Bellot <demis.bellot@gmail.com>
2017-08-12 22:21:33 -0400

    add more db filters

--- src/TemplateWebsites/DbTemplateFilters.cs
+++ src/TemplateWebsites/DbTemplateFilters.cs
@@ -26,6 +26,7 @@ namespace TemplateWebsites
         public object dbScalar(string sql, Dictionary<string,object> args) => exec(db => db.Scalar<object>(sql,args));
         public int dbExec(string sql, Dictionary<string,object> args) => exec(db => db.ExecuteSql(sql, args));
 
+        public string sqlQuote(string name) => OrmLiteConfig.DialectProvider.GetQuotedName(name);
         public string sqlConcat(IEnumerable<object> values) => OrmLiteConfig.DialectProvider.SqlConcat(values);
         public string sqlCurrency(string fieldOrValue) => OrmLiteConfig.DialectProvider.SqlCurrency(fieldOrValue);
         public string sqlCurrency(string fieldOrValue, string symbol) => OrmLiteConfig.DialectProvider.SqlCurrency(fieldOrValue, symbol);
@@ -33,5 +34,10 @@ namespace TemplateWebsites
         public string sqlBool(bool value) => OrmLiteConfig.DialectProvider.SqlBool(value);
         public string sqlTrue() => OrmLiteConfig.DialectProvider.SqlBool(true);
         public string sqlFalse() => OrmLiteConfig.DialectProvider.SqlBool(false);
+        public string sqlLimit(int? offset, int? limit) => padCondition(OrmLiteConfig.DialectProvider.SqlLimit(offset, limit));
+        public string sqlLimit(int? limit) => padCondition(OrmLiteConfig.DialectProvider.SqlLimit(null, limit));
+        public string sqlSkip(int? offset) => padCondition(OrmLiteConfig.DialectProvider.SqlLimit(offset, null));
+        public string sqlTake(int? limit) => padCondition(OrmLiteConfig.DialectProvider.SqlLimit(null, limit));
+        private string padCondition(string text) => string.IsNullOrEmpty(text) ? "" : " " + text;
     }
 }
