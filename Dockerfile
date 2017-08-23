4710954c061f1e24ef81690f634cf1cab6a31c16
Demis Bellot <demis.bellot@gmail.com>
2017-08-23 06:34:46 -0400

    add travis scripts

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM microsoft/dotnet:2.0-sdk
+COPY src /app
+WORKDIR /app/TemplateWebsites
+RUN ["dotnet", "restore", "--configfile", "NuGet.Config"]
+RUN ["dotnet", "build"]
+EXPOSE 5000/tcp
+ENV ASPNETCORE_URLS https://*:5000
+ENTRYPOINT ["dotnet", "run", "--server.urls", "http://*:5000"]
--- src/TemplateWebsites/run.deploy.apps.sh
+++ src/TemplateWebsites/run.deploy.apps.sh
@@ -1,12 +1,12 @@
-cat ../apps/fs-sqlite/web.settings | sed "/debug/s/ .*/ false/" | sed "/contentRoot/s/ .*/ ..\/apps/" | sed "/webRoot/s/ .*/ ..\/apps\/fs-sqlite/" \
-| sed "/port/s/ .*/ 5001/" > bin/Release/netcoreapp2.0/publish/fs-sqlite.web.settings
-
-cat ../apps/fs-sqlite/web.settings | sed "/debug/s/ .*/ false/" | sed "/contentRoot/s/ .*/ ..\/apps/" | sed "/webRoot/s/ .*/ ..\/apps\/fs-sqlite/" \
-| sed "/port/s/ .*/ 5001/" > bin/Release/netcoreapp2.0/publish/fs-sqlite.web.settings
-
-cat ../apps/fs-redis/web.settings | sed "/debug/s/ .*/ false/" | sed "/contentRoot/s/ .*/ ..\/apps/" | sed "/webRoot/s/ .*/ ..\/apps\/fs-redis/" \
-| sed "/port/s/ .*/ 5002/" > bin/Release/netcoreapp2.0/publish/fs-redis.web.settings
-
 rsync -avz -e 'ssh' bin/Release/netcoreapp2.0/publish/ deploy@gistlyn.com:/home/deploy/web
+
+cat ../apps/rockwind-fs/web.sqlite.settings | sed "/debug/s/ .*/ false/" | sed "/contentRoot/s/ .*/ ..\/apps/" | sed "/webRoot/s/ .*/ ..\/apps\/rockwind-fs/" \
+| sed "/port/s/ .*/ 5001/" > bin/Release/netcoreapp2.0/publish/rockwind-fs.web.sqlite.settings
+
+cat ../apps/rockwind-vfs/web.sqlite.settings | sed "/debug/s/ .*/ false/" | sed "/contentRoot/s/ .*/ ..\/apps/" | sed "/webRoot/s/ .*/ ..\/apps\/rockwind-vfs/" \
+| sed "/port/s/ .*/ 5001/" > bin/Release/netcoreapp2.0/publish/rockwind-vfs.web.sqlite.settings
+
+cat ../apps/redis/web.settings | sed "/debug/s/ .*/ false/" | sed "/contentRoot/s/ .*/ ..\/apps/" | sed "/webRoot/s/ .*/ ..\/apps\/redis/" \
+| sed "/port/s/ .*/ 5002/" > bin/Release/netcoreapp2.0/publish/redis.web.settings
 
 rsync -avz -e 'ssh' ../apps deploy@gistlyn.com:/home/deploy 
