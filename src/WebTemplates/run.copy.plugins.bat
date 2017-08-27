4023eed4f48bacd861b76d374232c7b22265d9a7
Demis Bellot <demis.bellot@gmail.com>
2017-08-27 03:04:23 -0400

    Finish web-plugins example app

--- src/WebTemplates/run.copy.plugins.bat
+++
@@ -1,5 +0,0 @@
-dotnet build -c Release
-dotnet publish -c Release
-
-RMDIR ..\apps\bin\ /s /q
-XCOPY /E bin\Release\netcoreapp2.0\publish ..\apps\bin\
--- src/apps/web-plugins/wwwroot/assets/js/default.js
+++ src/apps/web-plugins/wwwroot/assets/js/default.js
@@ -42,3 +42,47 @@ function queryStringParams(qs) {
     }
     return params;
 }
+
+$.fn.ajaxSubmit = function(opt) {
+    return this.each(function(){ 
+        $(this).submit(function(e){ 
+            e.preventDefault();
+
+            var f = $(this);
+            f.removeClass('is-invalid').find('.invalid-feedback').html('');
+
+            var data = {};
+            f.find("input").removeClass('is-invalid').each(function(){ data[this.name] = this.value });
+            $.ajax({ 
+                url: f.attr('action'),
+                method: "POST",
+                data: JSON.stringify(data),
+                contentType: 'application/json',
+                dataType: opt.dataType || 'json',
+                success: opt.success,
+                error: opt.error || function(jq,status,errMsg) { ajaxSubmitError(f, jq) }
+            })
+
+        }) 
+    });
+}
+
+function ajaxSubmitError(f, jqxhr) {
+    try {
+        var errorResponse = JSON.parse(jqxhr.responseText);
+        var status = errorResponse.responseStatus;
+        if (status) {
+            if (status.errors && status.errors.length > 0) {
+                for (var i=0; i<status.errors.length; i++) {
+                    var fieldError = status.errors[i];
+                    f.find("[name=" + fieldError.fieldName + "]").addClass('is-invalid').next('.invalid-feedback').html(fieldError.message);
+                }
+            }
+            else {
+                f.find('.error-summary').html('<div class="alert alert-danger">' + status.message + '</div>')
+            }
+            return;
+        }
+    } catch(e) {}
+    f.find('.error-summary').html('<div class="alert alert-danger"><pre>' + jqxhr.status + ' ' + jqxhr.statusText + '</pre></div>')
+}
