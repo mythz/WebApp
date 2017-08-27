da90e50b157ec2513a60cdb4b52fba916418bd7b
Demis Bellot <demis.bellot@gmail.com>
2017-08-27 03:36:02 -0400

    Update web binaries

--- src/WebTemplates/run.deploy.apps.sh
+++ src/WebTemplates/run.deploy.apps.sh
@@ -6,4 +6,6 @@ cat ../apps/rockwind-vfs/web.sqlite.settings | sed "/debug/s/ .*/ false/" | sed 
 
 cat ../apps/redis/web.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5003/" > ../apps/bin/web.redis.settings
 
+cat ../apps/web-plugins/web.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5004/" > ../apps/bin/web.web-plugins.settings
+
 rsync -avz -e 'ssh' ../apps deploy@gistlyn.com:/home/deploy 
