914dec3632e2876b15aa72bf9599a55058695252
Demis Bellot <demis.bellot@gmail.com>
2017-08-27 09:44:53 -0400

    gitignore run.deploy's apps/*.setting

--- src/WebTemplates/run.deploy.apps.sh
+++ src/WebTemplates/run.deploy.apps.sh
@@ -1,11 +1,11 @@
 # run.publish.bat with cmd.exe first
 
-cat ../apps/rockwind/web.sqlite.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5001/" > ../apps/bin/web.rockwind-sqlite.settings
+cat ../apps/rockwind/web.sqlite.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5001/" > ../apps/app/web.rockwind-sqlite.settings
 
-cat ../apps/rockwind-vfs/web.sqlite.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5002/" > ../apps/bin/web.rockwind-vfs-sqlite.settings
+cat ../apps/rockwind-vfs/web.sqlite.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5002/" > ../apps/app/web.rockwind-vfs-sqlite.settings
 
-cat ../apps/redis/web.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5003/" > ../apps/bin/web.redis.settings
+cat ../apps/redis/web.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5003/" > ../apps/app/web.redis.settings
 
-cat ../apps/web-plugins/web.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5004/" > ../apps/bin/web.web-plugins.settings
+cat ../apps/web-plugins/web.settings | sed "/debug/s/ .*/ false/" | sed "/port/s/ .*/ 5004/" > ../apps/app/web.web-plugins.settings
 
 rsync -avz -e 'ssh' ../apps deploy@gistlyn.com:/home/deploy 
--- src/WebTemplates/run.publish.bat
+++ src/WebTemplates/run.publish.bat
@@ -1,5 +1,5 @@
 dotnet build -c Release
 dotnet publish -c Release
 
-RMDIR ..\apps\bin\ /s /q
+RMDIR ..\apps\app\ /s /q
 XCOPY /E bin\Release\netcoreapp2.0\publish ..\apps\app\
