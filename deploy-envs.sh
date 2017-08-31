8505bec7fe991f2ec283d862471c467d57a7da39
xplicit <svg@ngs.ru>
2017-09-01 05:16:41 +0700

    Add scripts for publishing to Azure

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -8,6 +8,10 @@ export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=rockwind-aws.web-app.io
 
+export AZURE_REGISTRY=netcoreapps.azurecr.io
+
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_ID, AWS_ACCESS_KEY, AWS_SECRET_KEY
 # optional: SERVICESTACK_LICENSE
+
+# required: AZURE_REGISTRY_LOGIN, AZURE_REGISTRY_PASSWORD
--- scripts/build.sh
+++ scripts/build.sh
@@ -7,3 +7,6 @@ export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws
 # Build process
 docker build -t $IMAGE_NAME ../
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
+
+# Tag docker image for latter push to Azure Registry Container
+docker tag $IMAGE_NAME $AZURE_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION
