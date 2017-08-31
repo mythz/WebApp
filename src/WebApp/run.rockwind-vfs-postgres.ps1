abf7a464211019b2f43f8af7316c27e1c2f1862c
Demis Bellot <demis.bellot@gmail.com>
2017-08-31 00:26:37 -0400

    change contentRoot to match webRoot unless SQLite where it's needed

--- src/WebApp/run.rockwind-vfs-postgres.ps1
+++ src/WebApp/run.rockwind-vfs-postgres.ps1
@@ -1,6 +1,6 @@
 (Get-Content ..\apps\rockwind-vfs\web.postgres.settings) `
-    -replace 'contentRoot ~/..', 'contentRoot ~/../../../../apps' `
-    -replace 'webRoot ~/../rockwind', 'webRoot ~/../../../../apps/rockwind-vfs' `
+    -replace 'contentRoot ~/../rockwind-vfs', 'contentRoot ~/../../../../apps/rockwind-vfs' `
+    -replace 'webRoot ~/../rockwind-vfs', 'webRoot ~/../../../../apps/rockwind-vfs' `
     | Set-Content bin\Debug\netcoreapp2.0\web.settings
 
 dotnet run
--- src/WebApp/run.rockwind-vfs-sqlserver.ps1
+++ src/WebApp/run.rockwind-vfs-sqlserver.ps1
@@ -1,5 +1,5 @@
 (Get-Content ..\apps\rockwind-vfs\web.sqlserver.settings) `
-    -replace 'contentRoot ~/..', 'contentRoot ~/../../../../apps' `
+    -replace 'contentRoot ~/../rockwind-vfs', 'contentRoot ~/../../../../apps/rockwind-vfs' `
     -replace 'webRoot ~/../rockwind-vfs', 'webRoot ~/../../../../apps/rockwind-vfs' `
     | Set-Content bin\Debug\netcoreapp2.0\web.settings
 
